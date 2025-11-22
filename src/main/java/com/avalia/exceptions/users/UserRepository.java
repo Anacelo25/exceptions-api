abd0bc2779daeac336859bbfd87460248d646e85
Marcelo <marcelogemaque25@gmail.com>
2025-11-22 16:49:16 -0300

    feat: Adição de interface para classe usuário.

--- src/main/java/com/avalia/exceptions/users/UserRepository.java
+++ src/main/java/com/avalia/exceptions/users/UserRepository.java
@@ -1,4 +1,8 @@
 package com.avalia.exceptions.users;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Usuario, Long> {
 }
