2085554d9e04f628c6829f5bb66cc99abfc2c35a
Marcelo <marcelogemaque25@gmail.com>
2025-11-22 16:55:56 -0300

    feat: Adição de interface para classe usuário.

--- src/main/java/com/avalia/exceptions/users/UserRepository.java
+++ src/main/java/com/avalia/exceptions/users/UserRepository.java
@@ -1,8 +1,13 @@
 package com.avalia.exceptions.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Usuario, Long> {
+
+    @Query("SELECT u FROM Usuario u LEFT JOIN FETCH u.pedidos WHERE u.id = ?1")
+    Optional<Usuario> findUsuarioWithPedido(Long id);
 }
